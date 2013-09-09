f2006cfb129ffbc7c466cfb026c004f52ea17db5
Andy Pliszka <andy@isp44.com>
2013-09-09 16:15:18 -0400

    Wokring tonr2 image page

---
+++ src/main/java/org/energyos/espi/thirdparty/service/SparklrException.java
@@ -0,0 +1,11 @@
+package org.energyos.espi.thirdparty.service;
+
+/**
+ * @author Ryan Heaton
+ */
+public class SparklrException extends Exception {
+
+  public SparklrException(String message) {
+    super(message);
+  }
+}
