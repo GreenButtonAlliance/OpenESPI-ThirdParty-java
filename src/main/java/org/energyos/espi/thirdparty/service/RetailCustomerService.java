61d52c65ea35258a96899e35eefa1ab3cc0b5181
Andy Pliszka and Ian Zabel <pair+andy+izabel@pivotallabs.com>
2013-09-25 15:41:33 -0400

    Adds persist to RetailCustomerService

--- src/main/java/org/energyos/espi/thirdparty/service/RetailCustomerService.java
+++ src/main/java/org/energyos/espi/thirdparty/service/RetailCustomerService.java
@@ -1,8 +1,11 @@
 package org.energyos.espi.thirdparty.service;
 
+import org.energyos.espi.thirdparty.domain.RetailCustomer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface RetailCustomerService extends UserDetailsService {
     UserDetails loadUserByUsername(String username);
+
+    void persist(RetailCustomer retailCustomer);
 }
