d425fac2c4c306d31ebfd66941750f0ba5fe81e2
Andy Pliszka and Ian Zabel <pair+andy+izabel@pivotallabs.com>
2013-09-25 17:00:06 -0400

    Adds URL to DataCustodian seeds

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
-INSERT INTO data_custodians (id, description) VALUES (1, 'ConEdison');
+INSERT INTO data_custodians (id, description, url) VALUES (1, 'ConEdison', 'http://localhost:8080/DataCustodian/login');
+INSERT INTO data_custodians (id, description, url) VALUES (2, 'JCP&L', 'http://localhost:8080/DataCustodian/login');
 
 INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (1, 'alan',    'Alan',    'Turing',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
 INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (2, 'marissa', 'Marissa',  'Meyer',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
