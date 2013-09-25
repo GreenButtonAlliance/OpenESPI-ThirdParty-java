60f9fbf320d64784faa54a681f6165e8202dee74
Andy Pliszka and Ian Zabel <pair+andy+izabel@pivotallabs.com>
2013-09-25 15:48:29 -0400

    Adds DataCustodian findById to service

--- src/test/java/org/energyos/espi/thirdparty/service/impl/DataCustodianServiceImplTests.java
+++ src/test/java/org/energyos/espi/thirdparty/service/impl/DataCustodianServiceImplTests.java
@@ -1,6 +1,9 @@
 package org.energyos.espi.thirdparty.service.impl;
 
+import org.energyos.espi.thirdparty.domain.DataCustodian;
 import org.energyos.espi.thirdparty.repository.DataCustodianRepository;
+import org.energyos.espi.thirdparty.utils.factories.EspiFactory;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
@@ -8,12 +11,19 @@ import static org.mockito.Mockito.verify;
 
 public class DataCustodianServiceImplTests {
 
+    private DataCustodianServiceImpl service;
+    private DataCustodianRepository repository;
+
+    @Before
+    public void before() {
+        service = new DataCustodianServiceImpl();
+        repository = mock(DataCustodianRepository.class);
+        service.setRepository(repository);
+    }
+
+
     @Test
     public void findById() {
-        DataCustodianServiceImpl service = new DataCustodianServiceImpl();
-        DataCustodianRepository repository = mock(DataCustodianRepository.class);
-        service.setRepository(repository);
-
         service.findById(99L);
 
         verify(repository).findById(99L);
@@ -21,12 +31,17 @@ public class DataCustodianServiceImplTests {
 
     @Test
     public void findAll() {
-        DataCustodianServiceImpl service = new DataCustodianServiceImpl();
-        DataCustodianRepository repository = mock(DataCustodianRepository.class);
-        service.setRepository(repository);
-
         service.findAll();
 
         verify(repository).findAll();
+    }
+
+    @Test
+    public void persist() {
+        DataCustodian dataCustodian = EspiFactory.newDataCustodian();
+
+        service.persist(dataCustodian);
+
+        verify(repository).persist(dataCustodian);
     }
 }
