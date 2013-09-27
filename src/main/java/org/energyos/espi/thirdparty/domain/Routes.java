5045428af0dfa2d4057e8967bf83a5a5e5e5cba8
Andy Pliszka <pair+andy@pivotallabs.com>
2013-09-27 14:25:41 -0400

    Splits seed data for each Maven profile

--- src/main/java/org/energyos/espi/thirdparty/domain/Routes.java
+++ src/main/java/org/energyos/espi/thirdparty/domain/Routes.java
@@ -1,5 +1,11 @@
 package org.energyos.espi.thirdparty.domain;
 
 public class Routes {
+    public static final String AuthorizationServerAuthorizationEndpoint = "/RetailCustomer/ApproveAuthorization";
+
+    public static final String ThirdPartyScopeAuthorization = "/RetailCustomer/ScopeAuthorization";
     public static final String ThirdPartyScopeSelectionScreen = "/RetailCustomer/ScopeSelection";
+    public static final String ThirdPartyOAuthCodeCallbackURL = "/espi/1_1/resource/Authorization";
+
+    public static final String DataCustodianScopeSelectionScreen = "/RetailCustomer/ScopeSelection";
 }
