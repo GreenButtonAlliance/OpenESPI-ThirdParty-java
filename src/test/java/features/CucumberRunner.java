a42dad24538ae0369d7806fcc0393baf7118adbf
Ian Zabel and William Ramsey <pair+izabel+wramsey@pivotallabs.com>
2013-09-30 12:45:49 -0400

    Combine Cuke features into single runner

--- src/test/java/features/CucumberRunner.java
+++ src/test/java/features/CucumberRunner.java
@@ -20,6 +20,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(features = {"classpath:features/HomePage.feature"})
-public class HomePageFeature {
+@Cucumber.Options(features = "classpath:features/", tags = "~@oauth")
+public class CucumberRunner {
 }
