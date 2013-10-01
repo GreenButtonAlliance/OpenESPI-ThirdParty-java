dde5a391a7d6f7a3e1cb917b4fff5857b93fdaf9
Andy Pliszka <pair+andy@pivotallabs.com>
2013-10-01 17:32:43 -0400

    Replace Marissa with Donald in seed data

--- src/main/resources/db/dev-populateDB.sql
+++ src/main/resources/db/dev-populateDB.sql
@@ -2,6 +2,6 @@ INSERT INTO data_custodians (id, description, url) VALUES (1, 'JCP&L (localhost)
 INSERT INTO data_custodians (id, description, url) VALUES (2, 'ConEdison (localhost)', 'http://localhost:8080/DataCustodian');
 
 INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (1, 'alan',    'Alan',    'Turing',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
-INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (2, 'marissa', 'Marissa',  'Meyer',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
+INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (2, 'donald',  'Donald',  'Knuth',        'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
 INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (3, 'sam',     'Sam',      'White',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
 INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (4, 'grace',   'Grace',    'Hopper',      'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
