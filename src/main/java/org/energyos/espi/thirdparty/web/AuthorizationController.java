eca424ad1ec0b7ed70430f3da757ad3fdc2e8e7b
Andy Pliszka <pair+andy@pivotallabs.com>
2013-10-01 14:53:07 -0400

    Adds AuthorizationController

--- src/main/java/org/energyos/espi/thirdparty/web/AuthorizationController.java
+++ src/main/java/org/energyos/espi/thirdparty/web/AuthorizationController.java
@@ -16,32 +16,55 @@
 
 package org.energyos.espi.thirdparty.web;
 
+import org.energyos.espi.thirdparty.domain.Authorization;
+import org.energyos.espi.thirdparty.domain.Configuration;
 import org.energyos.espi.thirdparty.domain.Routes;
+import org.energyos.espi.thirdparty.service.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 
+import java.security.Principal;
+
 @Controller
-public class AuthorizationController {
+public class AuthorizationController extends BaseController {
+
+    @Autowired
+    private AuthorizationService service;
 
     @Autowired
     @Qualifier("clientRestTemplate")
     private RestTemplate template;
 
     @RequestMapping(value = Routes.ThirdPartyOAuthCodeCallbackURL, method = RequestMethod.GET)
-    public String authorization(String code) {
+    public String authorization(String code, ModelMap model, Principal principal) {
         String url = String.format("http://localhost:8080/DataCustodian%s?redirect_uri=%s&code=%s&grant_type=authorization_code",
                 Routes.AuthorizationServerTokenEndpoint, "http://localhost:8080/ThirdParty" + Routes.ThirdPartyOAuthCodeCallbackURL, code);
 
         String token = template.getForObject(url, String.class);
+
+        Authorization authorization = new Authorization();
+        authorization.setAccessToken(token);
+        authorization.setAuthorizationServer(Routes.AuthorizationServerAuthorizationEndpoint);
+        authorization.setThirdParty(Configuration.THIRD_PARTY_CLIENT_ID);
+        authorization.setRetailCustomer(currentCustomer(principal));
+
+        service.persist(authorization);
+
+        model.put("authorizationList", service.findAllByRetailCustomerId(currentCustomer(principal).getId()));
 
         return null;
     }
 
     public void setTemplate(RestTemplate template) {
         this.template = template;
+    }
+
+    public void setService(AuthorizationService service) {
+        this.service = service;
     }
 }
--- src/test/java/org/energyos/espi/thirdparty/web/AuthorizationControllerTests.java
+++ src/test/java/org/energyos/espi/thirdparty/web/AuthorizationControllerTests.java
@@ -16,26 +16,76 @@
 
 package org.energyos.espi.thirdparty.web;
 
+import org.energyos.espi.thirdparty.domain.Authorization;
+import org.energyos.espi.thirdparty.domain.RetailCustomer;
 import org.energyos.espi.thirdparty.domain.Routes;
+import org.energyos.espi.thirdparty.service.AuthorizationService;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class AuthorizationControllerTests {
 
+    private AuthorizationController controller;
+    private RestTemplate restTemplate;
+    private Authentication principal;
+    private AuthorizationService service;
+
+    @Before
+    public void before() {
+        controller = new AuthorizationController();
+
+        service = mock(AuthorizationService.class);
+        controller.setService(service);
+
+        restTemplate = mock(RestTemplate.class);
+        controller.setTemplate(restTemplate);
+
+        RetailCustomer retailCustomer = new RetailCustomer();
+        principal = mock(Authentication.class);
+        when(principal.getPrincipal()).thenReturn(retailCustomer);
+    }
+
     @Test
     public void authorization_fetchesToken() throws Exception {
-        AuthorizationController controller = new AuthorizationController();
-        RestTemplate restTemplate = mock(RestTemplate.class);
-        controller.setTemplate(restTemplate);
-        String code = "dC7BJ5";
         String url = String.format("http://localhost:8080/DataCustodian%s?redirect_uri=%s&code=%s&grant_type=authorization_code",
-                Routes.AuthorizationServerTokenEndpoint, "http://localhost:8080/ThirdParty" + Routes.ThirdPartyOAuthCodeCallbackURL, code);
+                Routes.AuthorizationServerTokenEndpoint, "http://localhost:8080/ThirdParty" + Routes.ThirdPartyOAuthCodeCallbackURL, "code");
 
-        controller.authorization(code);
+        controller.authorization("code", new ModelMap(), principal);
+
         verify(restTemplate).getForObject(eq(url), eq(String.class));
+    }
+
+    @Test
+    public void authorization_savesAuthorization() throws Exception {
+        String url = String.format("http://localhost:8080/DataCustodian%s?redirect_uri=%s&code=%s&grant_type=authorization_code",
+                Routes.AuthorizationServerTokenEndpoint, "http://localhost:8080/ThirdParty" + Routes.ThirdPartyOAuthCodeCallbackURL, "code");
+
+        controller.authorization("code", new ModelMap(), principal);
+
+        verify(service).persist(any(Authorization.class));
+    }
+
+    @Test
+    public void authorization_returnsAuthorizationList() throws Exception {
+        List<Authorization> authorizations = new ArrayList<Authorization>();
+        authorizations.add(new Authorization());
+        when(service.findAllByRetailCustomerId(anyLong())).thenReturn(authorizations);
+        ModelMap model = new ModelMap();
+
+        controller.authorization("code", model, principal);
+
+        assertEquals(authorizations, model.get("authorizationList"));
     }
 }
