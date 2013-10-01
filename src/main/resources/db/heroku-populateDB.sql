147158afe762118fd11951347469b55809a4a837
Ian Zabel and William Ramsey <pair+izabel+wramsey@pivotallabs.com>
2013-10-01 10:13:25 -0400

    Moves redundant SQL from profile specific SQL files to a common SQL file.

--- src/main/resources/db/heroku-populateDB.sql
+++ src/main/resources/db/heroku-populateDB.sql
@@ -1,8 +1,2 @@
 INSERT INTO data_custodians (id, description, url) VALUES (1, 'JCP&L (Heroku)', 'http://datacustodian-dev.herokuapp.com/DataCustodian/RetailCustomer/ScopeSelection');
 INSERT INTO data_custodians (id, description, url) VALUES (2, 'ConEdison (Heroku)', 'http://datacustodian-dev.herokuapp.com/DataCustodian/RetailCustomer/ScopeSelection');
-
-INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (1, 'alan',    'Alan',    'Turing',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
-INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (2, 'marissa', 'Marissa',  'Meyer',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
-INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (3, 'sam',     'Sam',      'White',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
-INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (4, 'grace',   'Grace',    'Hopper',      'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
-
