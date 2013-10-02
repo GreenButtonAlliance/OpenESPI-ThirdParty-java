479cf0b2e244a1b89d1b52cbd25f7d1ccc1ff2b0
Andy Pliszka <pair+andy@pivotallabs.com>
2013-10-02 14:43:38 -0400

    Rename ThirdParty clientId

--- src/main/java/org/energyos/espi/thirdparty/domain/Configuration.java
+++ src/main/java/org/energyos/espi/thirdparty/domain/Configuration.java
@@ -1,9 +1,10 @@
 package org.energyos.espi.thirdparty.domain;
 
 public class Configuration {
-    public static String THIRD_PARTY_CLIENT_ID = "tonr";
-    public static String[] SCOPES = new String [] {
+    public static final String THIRD_PARTY_BASE_URL = "http://localhost:8080/ThirdParty";
+    public static final String THIRD_PARTY_CLIENT_ID = "third_party";
+    public static final String THIRD_PARTY_CLIENT_PASSWORD = "secret";
+    public static final String[] SCOPES = new String [] {
             "FB_4_5_15_IntervalDuration_3600_BlockDuration_monthly_HistoryLength_13"
     };
-    public static String THIRD_PARTY_BASE_URL = "http://localhost:8080/ThirdParty";
 }
--- src/main/java/org/energyos/espi/thirdparty/web/ClientRestTemplate.java
+++ src/main/java/org/energyos/espi/thirdparty/web/ClientRestTemplate.java
@@ -4,6 +4,7 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.energyos.espi.thirdparty.domain.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,7 @@ public class ClientRestTemplate extends RestTemplate {
     public ClientRestTemplate() {
         DefaultHttpClient httpClient = new DefaultHttpClient();
         BasicCredentialsProvider credentialsProvider =  new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("tonr", "secret"));
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(Configuration.THIRD_PARTY_CLIENT_ID, Configuration.THIRD_PARTY_CLIENT_PASSWORD));
         httpClient.setCredentialsProvider(credentialsProvider);
         ClientHttpRequestFactory rf = new HttpComponentsClientHttpRequestFactory(httpClient);
 
