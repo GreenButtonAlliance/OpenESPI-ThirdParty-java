fa8b38f2fd049c9c3ec2376b0c028144e0284667
Andy Pliszka <pair+andy@pivotallabs.com>
2013-10-02 14:30:47 -0400

    Removes invalid characters from Scopes

--- src/main/java/org/energyos/espi/thirdparty/domain/Configuration.java
+++ src/main/java/org/energyos/espi/thirdparty/domain/Configuration.java
@@ -2,6 +2,8 @@ package org.energyos.espi.thirdparty.domain;
 
 public class Configuration {
     public static String THIRD_PARTY_CLIENT_ID = "tonr";
-    public static String[] SCOPES = new String [] { "FB=4,5,15 IntervalDuration=3600 BlockDuration=monthly HistoryLength=13", "FB=4,5,12,15,16 IntervalDuration=monthly BlockDuration=monthly HistoryLength=13" };
+    public static String[] SCOPES = new String [] {
+            "FB_4_5_15_IntervalDuration_3600_BlockDuration_monthly_HistoryLength_13"
+    };
     public static String THIRD_PARTY_BASE_URL = "http://localhost:8080/ThirdParty";
 }
