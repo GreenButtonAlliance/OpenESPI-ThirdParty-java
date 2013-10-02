7ec571f742ccda80c23de1b4aea9a32ccf648b2b
Andy Pliszka <pair+andy@pivotallabs.com>
2013-10-02 15:25:49 -0400

    Refactors authorization token URL

--- src/main/java/org/energyos/espi/thirdparty/domain/Routes.java
+++ src/main/java/org/energyos/espi/thirdparty/domain/Routes.java
@@ -2,7 +2,7 @@ package org.energyos.espi.thirdparty.domain;
 
 public class Routes {
     public static final String AuthorizationServerAuthorizationEndpoint = "/oauth/2_0/authorize";
-    public static final String AuthorizationServerTokenEndpoint = "/oauth/token";
+    public static final String AuthorizationServerTokenEndpoint = "/oauth/2_0/token";
 
     public static final String ThirdPartyScopeAuthorization = "/RetailCustomer/ScopeAuthorization";
     public static final String ThirdPartyScopeSelectionScreen = "/RetailCustomer/ScopeSelection";
