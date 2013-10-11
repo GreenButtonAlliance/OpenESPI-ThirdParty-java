d1bdde928dc68c5f44d40907b1ae15563d570ede
Andy Pliszka and Joshua Clark <pair+apliszka+jclark@pivotallabs.com>
2013-10-11 14:01:41 -0400

    Bugfix 401 during OAuth basic authorization

--- src/main/java/org/energyos/espi/thirdparty/web/ClientRestTemplate.java
+++ src/main/java/org/energyos/espi/thirdparty/web/ClientRestTemplate.java
@@ -1,23 +1,54 @@
 package org.energyos.espi.thirdparty.web;
 
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
 import org.energyos.espi.thirdparty.domain.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 public class ClientRestTemplate extends RestTemplate {
 
     public ClientRestTemplate() {
         DefaultHttpClient httpClient = new DefaultHttpClient();
         BasicCredentialsProvider credentialsProvider =  new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(Configuration.THIRD_PARTY_CLIENT_ID, Configuration.THIRD_PARTY_CLIENT_PASSWORD));
+        credentialsProvider.setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT), new UsernamePasswordCredentials(Configuration.THIRD_PARTY_CLIENT_ID, Configuration.THIRD_PARTY_CLIENT_PASSWORD));
         httpClient.setCredentialsProvider(credentialsProvider);
+        httpClient.addRequestInterceptor(new PreemptiveAuthInterceptor(), 0);
         ClientHttpRequestFactory rf = new HttpComponentsClientHttpRequestFactory(httpClient);
 
         this.setRequestFactory(rf);
+    }
+
+    static class PreemptiveAuthInterceptor implements HttpRequestInterceptor {
+
+        public void process(final HttpRequest request, final HttpContext context) throws HttpException, IOException {
+            AuthState authState = (AuthState) context.getAttribute(ClientContext.TARGET_AUTH_STATE);
+
+            if (authState.getAuthScheme() == null) {
+                CredentialsProvider credsProvider = (CredentialsProvider) context.getAttribute(ClientContext.CREDS_PROVIDER);
+                HttpHost targetHost = (HttpHost) context.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
+                Credentials creds = credsProvider.getCredentials(new AuthScope(targetHost.getHostName(), targetHost.getPort()));
+                if (creds == null)
+                    throw new HttpException("No credentials for preemptive authentication");
+                authState.setAuthScheme(new BasicScheme());
+                authState.setCredentials(creds);
+            }
+        }
     }
 }
