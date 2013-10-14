783e4966d9c0dc835d2daf5a6da1f3b9112147db
Josh Clark and William Ramsey <pair+jclark+wramsey@pivotallabs.com>
2013-10-14 10:20:18 -0400

    Fixes UsagePointRepository to work with UUID instead of MRID.

--- src/main/java/org/energyos/espi/thirdparty/repository/impl/UsagePointRepositoryImpl.java
+++ src/main/java/org/energyos/espi/thirdparty/repository/impl/UsagePointRepositoryImpl.java
@@ -55,7 +55,7 @@ public class UsagePointRepositoryImpl implements UsagePointRepository {
         List<UsagePoint> usagePoints = builder.newUsagePoints(unmarshallFeedType(requestUsagePoints()));
 
         for (UsagePoint usagePoint : usagePoints) {
-            if (usagePoint.getMRID().equals(usagePointId)) {
+            if (usagePoint.getUUID().toString().toLowerCase().equals(usagePointId.toLowerCase())) {
                 return usagePoint;
             }
         }
