1d0faa94f3faabffc3b8c5a6adb4d776f60a696b
Andy Pliszka and Brian Kelly <pair+andy+bkelly@pivotallabs.com>
2013-10-23 12:03:56 -0400

    Adds hashedId to RetailCustomer

--- src/main/java/org/energyos/espi/thirdparty/domain/RetailCustomer.java
+++ src/main/java/org/energyos/espi/thirdparty/domain/RetailCustomer.java
@@ -150,4 +150,8 @@ public class RetailCustomer implements UserDetails, Principal {
     public String getName() {
         return getUsername();
     }
+
+    public String getHashedId() {
+        return "" + getId();
+    }
 }
