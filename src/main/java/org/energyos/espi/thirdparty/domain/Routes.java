c171b1d0e9ac0e55dac0e335dc816978a9f89d4f
Andy Pliszka and Brian Kelly <pair+andy+bkelly@pivotallabs.com>
2013-10-24 09:27:03 -0400

    Adds notification and RESTful API URLs

--- src/main/java/org/energyos/espi/thirdparty/domain/Routes.java
+++ src/main/java/org/energyos/espi/thirdparty/domain/Routes.java
@@ -8,6 +8,12 @@ public class Routes {
     public static final String ThirdPartyScopeSelectionScreenWithRetailCustomerId = "/RetailCustomer/{retailCustomerId}/ScopeSelection";
     public static final String ThirdPartyOAuthCodeCallbackURL = "/espi/1_1/OAuthCallBack";
     public static final String ThirdPartyDataCustodianList = "/RetailCustomer/{retailCustomerID}/DataCustodianList";
+    public static final String ThirdPartyNotification = "/espi/1_1/Notification";
 
     public static final String DataCustodianScopeSelectionScreen = "/RetailCustomer/ScopeSelectionList";
+    public static final String DataCustodianRESTUsagePointGetURL = "/espi/1_1/resource/RetailCustomer/{RetailCustomerID}/UsagePoint/{UsagePointID}";
+
+    public static String getDataCustodianRESTUsagePointGetURL(String retailCustomerId, String usagePointId) {
+        return DataCustodianRESTUsagePointGetURL.replace("{RetailCustomerID}", retailCustomerId).replace("{UsagePointID}", usagePointId);
+    }
 }
