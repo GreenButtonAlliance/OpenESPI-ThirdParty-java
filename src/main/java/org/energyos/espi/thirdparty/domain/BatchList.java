ac1ab97bf05f51141adc49da6cd0d998da2634d4
Brian Kelly and Joshua Clark <pair+bkelly+jclark@pivotallabs.com>
2013-10-31 17:15:29 -0400

    Makes BatchList play with Hibernate and JAXB

--- src/main/java/org/energyos/espi/thirdparty/domain/BatchList.java
+++ src/main/java/org/energyos/espi/thirdparty/domain/BatchList.java
@@ -1,17 +1,40 @@
 package org.energyos.espi.thirdparty.domain;
 
-import java.io.Serializable;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BatchList implements Serializable {
+@XmlRootElement(name = "BatchList")
+@Entity
+public class BatchList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @XmlTransient
+    private Long id;
+
+    @ElementCollection
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @CollectionTable(name = "resources", joinColumns=@JoinColumn(name="id"))
+    @Column(name = "uri")
+    @XmlElement(name = "resource")
     private List<String> resources = new ArrayList<>();
 
     public List<String> getResources() {
         return resources;
     }
 
-    public void setResources(List<String> resources) {
-        this.resources = resources;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
