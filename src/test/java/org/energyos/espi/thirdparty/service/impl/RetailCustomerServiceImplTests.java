b6b52179df7b257a781955badc971c5bec654010
Andy Pliszka and Joshua Clark <pair+andy+jclark@pivotallabs.com>
2013-11-04 17:44:59 -0500

    Adds findById and findAll to RetailCustomerService

--- src/test/java/org/energyos/espi/thirdparty/service/impl/RetailCustomerServiceImplTests.java
+++ src/test/java/org/energyos/espi/thirdparty/service/impl/RetailCustomerServiceImplTests.java
@@ -2,19 +2,28 @@ package org.energyos.espi.thirdparty.service.impl;
 
 import org.energyos.espi.thirdparty.domain.RetailCustomer;
 import org.energyos.espi.thirdparty.repository.RetailCustomerRepository;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class RetailCustomerServiceImplTests {
+    private RetailCustomerRepository repository;
+    private RetailCustomerServiceImpl service;
+
+    @Before
+    public void setup() {
+        repository = mock(RetailCustomerRepository.class);
+        service = new RetailCustomerServiceImpl();
+        service.setRepository(repository);
+    }
 
     @Test
     public void loadUserByUsername() {
-        RetailCustomerRepository repository = mock(RetailCustomerRepository.class);
-        RetailCustomerServiceImpl service = new RetailCustomerServiceImpl();
-        service.setRepository(repository);
-
         service.loadUserByUsername("alan");
 
         verify(repository).findByUsername("alan");
@@ -22,13 +31,29 @@ public class RetailCustomerServiceImplTests {
 
     @Test
     public void persist() {
-        RetailCustomerRepository repository = mock(RetailCustomerRepository.class);
-        RetailCustomerServiceImpl service = new RetailCustomerServiceImpl();
-        service.setRepository(repository);
         RetailCustomer retailCustomer = new RetailCustomer();
 
         service.persist(retailCustomer);
 
         verify(repository).persist(retailCustomer);
+    }
+
+    @Test
+    public void findAll_returnsAllRetailCustomers() {
+        List<RetailCustomer> allRetailCustomers = new ArrayList<RetailCustomer>();
+
+        when(repository.findAll()).thenReturn(allRetailCustomers);
+
+        assertEquals(allRetailCustomers, service.findAll());
+    }
+
+    @Test
+    public void findById_returnsRetailCustomers() {
+        RetailCustomer customer = new RetailCustomer();
+        customer.setId(13L);
+
+        when(repository.findById(customer.getId())).thenReturn(customer);
+
+        assertEquals(customer, service.findById(customer.getId()));
     }
 }
