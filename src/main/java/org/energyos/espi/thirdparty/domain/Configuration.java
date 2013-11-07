a70c8224e2349b52733cdce3ab1dcc91d0685708
Brian Kelly and Joshua Clark <pair+bkelly+jclark@pivotallabs.com>
2013-11-07 14:33:49 -0500

    Modifies scopes to conform to new format

--- src/main/java/org/energyos/espi/thirdparty/domain/Configuration.java
+++ src/main/java/org/energyos/espi/thirdparty/domain/Configuration.java
@@ -5,7 +5,7 @@ public class Configuration {
     public static final String THIRD_PARTY_CLIENT_ID = "third_party";
     public static final String THIRD_PARTY_CLIENT_PASSWORD = "secret";
     public static final String[] SCOPES = new String [] {
-            "FB_4_5_15_IntervalDuration_3600_BlockDuration_monthly_HistoryLength_13",
-            "FB_4_5_15_IntervalDuration_3600_BlockDuration_monthly_HistoryLength_10"
+            "FB=4_5_15;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=13",
+            "FB=4_5_15;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=10"
     };
 }
