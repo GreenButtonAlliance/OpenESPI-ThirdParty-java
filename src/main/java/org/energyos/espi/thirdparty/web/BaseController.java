5c5419574400cb0b569589a522ba6fadfdd637f9
Ian Zabel and Josh Clark <pair+izabel+jclark@pivotallabs.com>
2013-11-19 15:07:05 -0500

    Adds role utility methods to BaseController

--- src/main/java/org/energyos/espi/thirdparty/web/BaseController.java
+++ src/main/java/org/energyos/espi/thirdparty/web/BaseController.java
@@ -7,6 +7,24 @@ import java.security.Principal;
 
 public class BaseController {
     public RetailCustomer currentCustomer(Principal principal) {
-        return (RetailCustomer) ((Authentication)principal).getPrincipal();
+        if (principal == null) return null;
+        return (RetailCustomer)((Authentication)principal).getPrincipal();
+    }
+
+    public boolean isUserCustodian(Principal principal) {
+        return checkRole(principal, RetailCustomer.ROLE_CUSTODIAN);
+    }
+
+    public boolean isUserUserRole(Principal principal) {
+        return checkRole(principal, RetailCustomer.ROLE_USER);
+    }
+
+    private boolean checkRole(Principal principal, String givenRole) {
+        RetailCustomer retailCustomer = currentCustomer(principal);
+        if(retailCustomer == null) return false;
+
+        String role = retailCustomer.getRole();
+
+        return givenRole.equals(role);
     }
 }
