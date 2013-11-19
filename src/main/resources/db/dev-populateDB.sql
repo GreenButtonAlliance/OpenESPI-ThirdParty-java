092d3a48c0542c8040bd617141758cc4017bbc36
Ian Zabel and Josh Clark <pair+izabel+jclark@pivotallabs.com>
2013-11-19 13:55:49 -0500

    Combines ROLE_USER into ROLE_CUSTOMER

--- src/main/resources/db/dev-populateDB.sql
+++ src/main/resources/db/dev-populateDB.sql
@@ -1,6 +1,6 @@
 INSERT INTO data_custodians (id, description, client_id, url) VALUES (1, 'ConEdison (localhost)', 'data_custodian', 'http://localhost:8080/DataCustodian');
 
-INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (1, 'alan',    'Alan',    'Turing',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
-INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (2, 'donald',  'Donald',  'Knuth',        'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
-INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (3, 'sam',     'Sam',      'White',       'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
-INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (4, 'grace',   'Grace',    'Hopper',      'koala', 1, 'ROLE_USER,ROLE_CUSTOMER');
+INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (1, 'alan',    'Alan',    'Turing',       'koala', 1, 'ROLE_CUSTOMER');
+INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (2, 'donald',  'Donald',  'Knuth',        'koala', 1, 'ROLE_CUSTOMER');
+INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (3, 'sam',     'Sam',      'White',       'koala', 1, 'ROLE_CUSTOMER');
+INSERT INTO retail_customers (id, username, first_name, last_name, password, enabled, role) VALUES (4, 'grace',   'Grace',    'Hopper',      'koala', 1, 'ROLE_CUSTOMER');
