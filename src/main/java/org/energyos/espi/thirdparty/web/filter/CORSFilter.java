2529b0fd67e2691ce97eb9411bd43089c872315e
Donald F Coffin <donald.coffin@reminetworks.com>
2013-12-16 19:48:31 -0800

    Rename OriginFilter to CORSFilter and add logger.debug statements

---
+++ src/main/java/org/energyos/espi/thirdparty/web/filter/CORSFilter.java
@@ -0,0 +1,29 @@
+package org.energyos.espi.thirdparty.web.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    	
+        if (logger.isDebugEnabled()) {
+            logger.debug("Request Header is '" + request.getHeader("Access-Control-Request-Method") + "'");
+            logger.debug("Request Header Method is '" + request.getMethod() + "'");            
+        }
+        
+    	response.addHeader("Access-Control-Allow-Origin", "*");        
+    	response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    	response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    	response.addHeader("Access-Control-Max-Age", "1800");
+
+        filterChain.doFilter(request, response);
+    }
+}
